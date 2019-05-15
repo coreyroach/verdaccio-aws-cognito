0a802d09b67b28a2ee9f39b48b6e06c650387299
Corey Allen Roach <car18@ntrs.com>
2019-05-15 15:41:12 -0500

    Fixed eslint issues.

--- src/awscognito.ts
+++ src/awscognito.ts
@@ -1,75 +1,69 @@
-import { IPluginAuth, Callback, Config, AuthConf } from "@verdaccio/types";
+import { IPluginAuth, Callback, Config, AuthConf } from '@verdaccio/types';
 import {
   AuthenticationDetails,
   CognitoUser,
   CognitoUserPool,
-  ICognitoUserPoolData,
+  ICognitoUserPoolData
 } from 'amazon-cognito-identity-js';
 
 // @ts-ignore
 global.fetch = require('node-fetch');
 
 export interface VerdaccioConfigApp extends Config {
-  userPoolId: string,
-  clientId: string,
+  userPoolId: string;
+  clientId: string;
 }
 
 export default class AWSCognito implements IPluginAuth<VerdaccioConfigApp> {
-  private userPool: any;
+  _userPool: CognitoUserPool;
 
-  constructor(config: AuthConf, stuff: VerdaccioConfigApp) {
+  constructor(config: AuthConf) {
     const userPoolData: ICognitoUserPoolData = {
       UserPoolId: config.userPoolId,
-      ClientId: config.clientId,
+      ClientId: config.clientId
     };
-    this.userPool = new CognitoUserPool(userPoolData);
+    this._userPool = new CognitoUserPool(userPoolData);
   }
 
-  authenticate(user: string, password: string, cb: Callback) {
+  authenticate(user: string, password: string, cb: Callback): void {
     const authData = {
       Username: user,
-      Password: password,
+      Password: password
     };
     const authDetails = new AuthenticationDetails(authData);
     const userData = {
       Username: user,
-      Pool: this.userPool,
+      Pool: this._userPool
     };
     const cognitoUser = new CognitoUser(userData);
     cognitoUser.authenticateUser(authDetails, {
-      onSuccess: (result) => {
-        console.log('Success', result);
+      onSuccess: (): void => {
         cb(null, [user]);
       },
-      newPasswordRequired: (attr, req) => {
-        console.log(attr);
-        console.log(req);
-        // TODO: Capture new password from form...
-        // cognitoUser.completeNewPasswordChallenge('<password>', req, {
-        //   onSuccess: (res) => {
-        //     console.log(res);
-        //     cb(null, false)
-        //   },
-        //   onFailure: (err) => {
-        //     console.log(err);
-        //     cb(null, false);
-        //   }
-        // });
-      },
-      onFailure: (err) => {
-        console.log('Error', err);
+      // TODO: Capture new password from form...
+      // newPasswordRequired: (attr, req) => {
+      //   console.log(attr);
+      //   console.log(req);
+      //   cognitoUser.completeNewPasswordChallenge('<password>', req, {
+      //     onSuccess: (res) => {
+      //       console.log(res);
+      //       cb(null, false)
+      //     },
+      //     onFailure: (err) => {
+      //       console.log(err);
+      //       cb(null, false);
+      //     }
+      //   });
+      // },
+      onFailure: (): void => {
         cb(null, false);
       }
     });
   }
 
-  adduser(user: string, password, cb: Callback) {
+  adduser(user: string, password, cb: Callback): void {
     this.authenticate(user, password, cb);
   }
 
-  changePassword() {}
-
-  register_middlewares(config, stuff, app, auth, storage) {
-    console.log('this.register_middlewares', auth);
-  }
+  changePassword(): void {}
 }
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import AWSCognito from './awscognito';
 /**
  * A new instance of AWSCognito class.
  * @param {object} config
- * @param {object} stuff
  * @returns {object}
  */
-export default function(config, stuff) {
-  return new AWSCognito(config, stuff);
+export default function(config): AWSCognito {
+  return new AWSCognito(config);
 }
